b86dcd390d5f6d0535b7c216458e0c809db2b8f2
Spencer Churchill <churchill@ionq.co>
2024-04-21 18:14:26 -0400

    speed up bar

--- basic_bar/basic_bar.py
+++ basic_bar/basic_bar.py
@@ -1,10 +1,15 @@
 import time, sys
 
-def bar(iterable, length=33):
+
+def progress(iterable, length=33):
     total, start = len(iterable), time.monotonic()
     for count, it in enumerate(iterable, 1):
         yield it
-        elapsed_time, remaining_time = time.monotonic() - start, (time.monotonic() - start) / count * (total - count)
-        percent, filled_len = count / total, int(length * (count / total))
-        sys.stdout.write(f'\r▕{"█" * filled_len}{" " * (length - filled_len)}▏ {percent*100:.2f}% {remaining_time:.2f}s'), sys.stdout.flush()
+        if count % max(1, total // 10000) == 0 or count == total:
+            percent = count / total
+            sys.stdout.write(
+                f'\r▕{"█" * int(length * percent) + " " * (length - int(length * percent))}▏ '
+                f"{percent * 100:.2f}% "
+                f"{(time.monotonic() - start) / count * (total - count):.2f}s"
+            ), sys.stdout.flush()
     sys.stdout.write(f'\r▕{"█" * length}▏ 100.00% {time.monotonic() - start:.2f}s\n')
